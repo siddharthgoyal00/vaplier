49796407b0bbaf80523f17c7a79c8765706e1bc4
siddharth goyal <113788213+siddharthgoyal00@users.noreply.github.com>
2025-12-03 20:10:12 +0530

    trpc setup

--- vaplier/src/app/page.tsx
+++ vaplier/src/app/page.tsx
@@ -1,16 +1,26 @@
-import prisma from "@/lib/db";
-import { Button } from "../components/ui/button"
+"use-client ";
+
+import { useQuery } from "@tanstack/react-query";
+
 // page.tsx default is a server component it wont accept the hooks like useEffect() 
-const Page = async ()=>{
-  const users = await prisma.user.findMany();
+
+
+
+const Page =  ()=>{
+     const trpc:any = useTrpc();
+     const {data:users} = useQuery(trpc.getUsers.queryOptions());
+
   return (
-    <div className="text-red-700 ">
-      hello world
+    <div className="text-red-700 justify-center items-center flex min-w-screen min-h-screen">
+     siddha
       {JSON.stringify(users)}
-      <Button >click me 
-      </Button> 
+     
     </div>
   )
  };
 
  export default Page ;
+function useTrpc() {
+  throw new Error("Function not implemented.");
+}
+
