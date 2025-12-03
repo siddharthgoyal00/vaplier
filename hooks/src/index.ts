2741822b4e376bed31f06403ec25dbf6ca549b6b
siddharth goyal <113788213+siddharthgoyal00@users.noreply.github.com>
2025-12-03 14:51:26 +0530

    prisma setup done

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -1,14 +1,35 @@
 import express from "express";
+import {Prisma, PrismaClient} from "@prisma/client"
+const client = new PrismaClient();
 const app = express();
+
 
 // https:// hooks.zapier.com/hooks/catch/23467816/187561
 // password logic
 
-app.post("/hooks.catch/:userId/:zapId", (req, res) => {
+app.post("/hooks.catch/:userId/:zapId", async(req, res) => {
     const userId = req.params.userId;
     const zapId = req.params.zapId ;
+    const body = req.body ;
 
     // store in db a new trigger 
-    // push it on to a queue (kafka/redis) 
+
+    await client.$transaction(async (tx: Prisma.TransactionClient)  =>{
+        const run = await tx.zapRun.create({
+        data : {
+            zapId: zapId,
+            metadata : body
+        }
+        });; 
+        await tx.zapRunOutbox.create({
+            data: {
+                zapRunId :run.id
+            }
+        })
+    }) 
+    res.json({
+        message : "Webhook received " 
+    })
     
 })
+app.listen(3002);
---
+++ vaplier/src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "../components/ui/button"
+
+const Page = ()=>{
+  return (
+    <div className="text-red-700 ">
+      hello world
+      <Button >click me 
+        </Button> 
+    </div>
+  )
+ };
+
+ export default Page ;
