e9e1cdcdec8d6ee4d80c156cace6315855342740
siddharth goyal <113788213+siddharthgoyal00@users.noreply.github.com>
2025-12-05 17:25:55 +0530

    trpc-setup-done

--- vaplier/src/app/page.tsx
+++ vaplier/src/app/page.tsx
@@ -1,26 +1,25 @@
-"use-client ";
 
-import { useQuery } from "@tanstack/react-query";
 
 // page.tsx default is a server component it wont accept the hooks like useEffect() 
 
+import { getQueryClient, trpc } from '@/trpc/server';
+import { Client } from './client';
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
+import { Suspense } from 'react';
 
-
-const Page =  ()=>{
-     const trpc:any = useTrpc();
-     const {data:users} = useQuery(trpc.getUsers.queryOptions());
-
+const Page =  async()=>{
+    const queryClient = getQueryClient();
+    void queryClient.prefetchQuery(trpc.getUsers.queryOptions());
   return (
     <div className="text-red-700 justify-center items-center flex min-w-screen min-h-screen">
-     siddha
-      {JSON.stringify(users)}
+     <HydrationBoundary state = {dehydrate(queryClient)}>
+        <Suspense fallback={<p>Loading...</p>}>
+          <Client />
+        </Suspense>
+     </HydrationBoundary>
      
     </div>
   )
  };
 
  export default Page ;
-function useTrpc() {
-  throw new Error("Function not implemented.");
-}
-
