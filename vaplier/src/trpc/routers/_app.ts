d89db7d62c9c027518e0575b74df2cb91e7a7335
siddharth goyal <113788213+siddharthgoyal00@users.noreply.github.com>
2025-12-08 23:00:48 +0530

    inngest-setup for background-jobs

--- vaplier/src/trpc/routers/_app.ts
+++ vaplier/src/trpc/routers/_app.ts
@@ -1,16 +1,22 @@
 
+import { inngest } from '@/inngest/client';
 import { protectedProcedure, createTRPCRouter } from '../init';
 import prisma from '@/lib/db';
 export const appRouter = createTRPCRouter({
 
-  getUsers: protectedProcedure.query(({ctx}) => {
-    //console.log({userId: ctx.auth.user.id});
-      return prisma.user.findMany({
-        where : {
-          id: ctx.auth.user.id,
-        }
-      });
+  getWorkflows: protectedProcedure.query(({ctx}) => {
+    
+      return prisma.workflow.findMany()
     }),
+    createWorkflow: protectedProcedure.mutation(async() => {
+      await inngest.send({
+        name: "test/hello.world",
+        data: {
+          email: "test@example.com",
+        },
+      });
+      return {success: true, message: "job Queued"}
+    })
 });
-// export type definition of API
+
 export type AppRouter = typeof appRouter;
